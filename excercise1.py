3bc44b7a6b350572076e4a8c60b3550f28bb7c26
mazelbr <maze.lbr@gmail.com>
2019-05-07 22:29:36 +0200

    Add files via upload

---
+++ excercise1.py
@@ -0,0 +1,8 @@
+print("Hello Excercise")
+print("Die Antwort: " + str(42) + "\n")
+
+var1 , var2 = float(input("Geben Sie zwei Zahlen ein")), float(input())
+print(var1==var2)
+print(var1>var2)
+print(var1==int(var1))
+print(var2!=int(var2))
